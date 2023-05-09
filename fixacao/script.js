4720928404439f42e5df7a3d5fae3417bc560bc0
BenjaminMMRuiz <benjaminmartinhomoyaruiz@gmail.com>
2023-05-09 12:19:49 +0100

    Exercicio em aula

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,18 @@
 // Resolva o Exercício de Fixação!
+
+const cartaDeConducao = (nome, anoDeNascimento, anoAtual) => {
+   
+    let anoDeNascimento - anoAtual 
+    let confirmacaoDeIdade = idade >= 18
+    
+
+    return `${nome} é maior de idade ${confirmacaoDeIdade}.`
+    
+    }
+    
+    console.log(confirmacaoDeDados("Benjamin", 1989, 2023))
+    console.log(confirmacaoDeDados("Carol", 1988, 2023))
+    
+
+            
+
